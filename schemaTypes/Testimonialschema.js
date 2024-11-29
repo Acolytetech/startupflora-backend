0fecb2743865798c3fb60753ca30fa6c8e2200a7
Acolytetech <slwaniya03@gmail.com>
2024-11-29 13:40:40 +0530

    testimonial varosel schema add

---
+++ schemaTypes/Testimonialschema.js
@@ -0,0 +1,44 @@
+import { defineField, defineType } from "sanity";
+
+export const Testimonials = defineType ({
+    name: "testimonial",
+    title: "Testimonial",
+    type: "document",
+    fields: [
+        defineField({
+        name: "name",
+        title: "Name",
+        type: "string",
+        description: "Name of the person giving the testimonial",
+      }),
+      defineField({
+        name: "image",
+        title: "Image",
+        type: "image",
+        description: "Author's image",
+        options: {
+          hotspot: true,
+        },
+      }),
+      defineField({
+        name: "role",
+        title: "Role",
+        type: "string",
+        description: "Role or position of the author",
+      }),
+      defineField({
+        name: "text",
+        title: "Quote",
+        type: "string",
+        description: "Short testimonial quote",
+      }),
+      defineField({
+        name: "details",
+        title: "Details",
+        type: "text",
+        description: "Detailed description of the testimonial",
+      }),
+   
+    ],
+  });
+  
--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -2,5 +2,6 @@ import { Footer } from "./Footer";
 import { Herosection } from "./Herosection";
 import { Navbar } from "./Navbar";
 import { Ourpartners } from "./Ourpartners";
+import { Testimonials } from "./Testimonialschema";
 
-export const schemaTypes = [Herosection , Navbar,Ourpartners ,Footer]
+export const schemaTypes = [Herosection , Navbar, Testimonials,Ourpartners ,Footer]
